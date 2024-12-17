d73c68aae35f09e29eaaa7e0e623aa500d09b875
Shimaa Awad <shimaaawad2023@gmail.com>
2024-12-17 05:57:21 -0800

    Your updated commit message

--- src/components/3-main/api.js
+++ src/components/3-main/api.js
@@ -2,30 +2,30 @@
     title:"E-Commerce",
     discription:"website-E-commerce used tools React-js-Redux",
     category:"React",
-    imgPath:"../../../public/e-commerce.png",
+    imgPath:"/public/e-commerce.png",
   },
   {
     title:"Website-puilding",
     discription:"Website-puilding used tools React-js-Redux",
     category:"React",
-    imgPath:"../../../public/puliding.png",
+    imgPath:"/public/puliding.png",
   },
   {
     title:"Website-Coffe",
     discription:"Coffe used tools React-js-Redux",
     category:"React",
-    imgPath:"../../../public/caffe.png",
+    imgPath:"/public/caffe.png",
   },
   {
     title:"Website Movie-star",
     discription:"Movie-star used tools React-js-Redux",
     category:"React",
-    imgPath:"../../../public/movie.png",
+    imgPath:"/public/movie.png",
   },
   {
     title:"Website lamp",
     discription:"Coffe used tools JS,Swpier",
     category:"Js",
-    imgPath:"../../../public/lamp.png",
+    imgPath:"/public/lamp.png",
   }
   ]
